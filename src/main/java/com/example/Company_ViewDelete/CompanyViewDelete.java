5c6045a455c7940fef1bdaad359aa36ecdd9933e
yby1997 <frmpjmattlemage@gmail.com>
2022-04-30 22:22:03 -0700

    added eureka client

--- src/main/java/com/example/Company_ViewDelete/CompanyViewDelete.java
+++ src/main/java/com/example/Company_ViewDelete/CompanyViewDelete.java
@@ -2,8 +2,10 @@ package com.example.Company_ViewDelete;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CompanyViewDelete {
 
 	public static void main(String[] args) {
